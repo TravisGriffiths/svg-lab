28bb58be8dd4a6f6167a43dfa5b174bd4b735168
Travis Griffiths <travis@travisgriffiths.com>
2023-03-04 15:00:52 -0500

    Resume mostly complete

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {	       
+			"primary": "#0055a2",	
+			"secondary": "#4B8F8C",	
+			"accent": "#ECA400",	
+			"neutral": "#191D24",	
+			"base-100": "#899878",	
+			"info": "#7272AC",	
+			"success": "#6A8D73",	
+			"warning": "#DCC48E",	
+			"error": "#7C1325",
+		},
+		fontFamily: {
+			sans: ['Open Sans', 'sans-serif'],
+			serif: ['Georgia', 'serif'],
+		  },
 		extend: {},
 	},
 	plugins: [],
